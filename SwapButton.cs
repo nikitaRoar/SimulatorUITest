74819362aa86994670ad63a9d4a71b36da8698ef
nikitaRoar <94848221+nikitaRoar@users.noreply.github.com>
2021-11-26 15:59:39 +0200

    Add files via upload

---
+++ SwapButton.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwapButton : MonoBehaviour
+{
+    public Sprite pic1;
+    public Sprite pic2;
+
+    private Image im;
+    void Start()
+    {
+        im = GetComponent<Image>();
+        im.sprite = pic1;
+    }
+    public void Swap()
+    {
+        if (im.sprite == pic1)
+        {
+            im.sprite = pic2;
+            return;
+        }
+
+        if (im.sprite == pic2)
+        {
+            im.sprite = pic1;
+            return;
+        }
+    }
+}
